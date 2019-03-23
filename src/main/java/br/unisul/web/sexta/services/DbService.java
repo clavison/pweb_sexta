e8250d8bd4e01fced6033678657c806044fe927d
clavison <clavison@gmail.com>
2019-03-22 21:01:58 -0300

    Endpoint Produtos

--- src/main/java/br/unisul/web/sexta/services/DbService.java
+++ src/main/java/br/unisul/web/sexta/services/DbService.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Service;
 
 import br.unisul.web.sexta.domain.Categoria;
 import br.unisul.web.sexta.domain.Estado;
+import br.unisul.web.sexta.domain.Produto;
 import br.unisul.web.sexta.repositories.CategoriaRepository;
 import br.unisul.web.sexta.repositories.EstadoRepository;
+import br.unisul.web.sexta.repositories.ProdutoRepository;
 
 @Service
 public class DbService {
@@ -20,6 +22,9 @@ public class DbService {
 	@Autowired
 	private EstadoRepository estRep;
 	
+	@Autowired
+	private ProdutoRepository prodRep;
+	
 	public void inicializaBancoDeDados() throws ParseException {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -29,12 +34,23 @@ public class DbService {
 		Categoria cat5 = new Categoria(null, "Jardinagem");
 		Categoria cat6 = new Categoria(null, "Decoração");
 		Categoria cat7 = new Categoria(null, "Perfumaria");
-		catRep.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		
 		Estado e1 = new Estado(null, "Paraná");
 		Estado e2 = new Estado(null, "Santa Catarina");
 		Estado e3 = new Estado(null, "Rio Grande do Sul");
 		estRep.saveAll(Arrays.asList(e1,e2,e3));
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		catRep.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		prodRep.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
